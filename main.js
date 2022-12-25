9a0dab2b08c47d5bb4c9c12a20e037090ef1af7c
Abhay08010 <110532944+Abhay08010@users.noreply.github.com>
2022-12-25 10:10:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+    var jingle_bells = new Audio('jingle-bells-christmas-hip-hop-128137.mp3');
+    
+    function start(){
+        jingle_bells.play();
+    }
+
+    function stop(){
+        jingle_bells.pause();
+    }
